2fed345cdf825c543a0e90e249edd0dfd9ed170b
Oscar Rodriguez <oscarrobertrodriguez@gmail.com>
2016-10-25 02:13:45 -0500

    made changes

--- cart.js
+++ cart.js
@@ -250,9 +250,14 @@ function displayInCart() {
 
 // remove object when the x symbol is pressed for that item
 function remove(ele) {
+   if (cart.promoMemory1.length > 0 || cart.promoMemory2.length > 0 || cart.promoMemory3.length > 0) {
+      alert("Please reapply your discount code.")
+   }
    var product = ele.parentNode.previousSibling.previousSibling.previousSibling;
    var productName = product.firstChild.innerHTML;
-
+   cart.promoMemory1.splice(0,1);
+   cart.promoMemory2.splice(0,1);
+   cart.promoMemory3.splice(0,1);
 
    var cartObject = utilities.findByProductName(cart.itemsCart, productName);
 
