751a96fd81bd35f7b9d2c3e6784f70dcdc5d3781
Oscar Rodriguez <oscarrobertrodriguez@gmail.com>
2016-10-26 11:22:36 -0500

    Now you can copy in paste code from top to input

--- cart.js
+++ cart.js
@@ -333,7 +333,7 @@ function incrementValue(ele) {
 function calculatePromo(el) {
    var parent = el.parentNode;
    var input = parent.previousSibling.previousSibling.firstChild.nextSibling;
-   var inputValue = input.value.toUpperCase();
+   var inputValue = input.value.toUpperCase().toUpperCase();
    var totalPricePromo = Number(document.getElementById("checkoutPrice").innerHTML.replace("$", "").replace("USD", "").trim());
 
    var totalPrice = document.getElementById("checkoutPrice");
